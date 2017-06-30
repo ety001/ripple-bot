1479b82ff4858e75660c106daf239ecf104d71b7
ETY001 <ety001@domyself.me>
2017-07-01 01:15:06 +0800

    remove setting temporary

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -3,7 +3,7 @@
     <el-col :span="24">
       <el-menu theme="dark" :default-active="activeIndex" :router="true" class="el-menu-demo" mode="horizontal" @select="handleSelect">
         <el-menu-item index="/">首页</el-menu-item>
-        <el-menu-item index="setting">设置</el-menu-item>
+        <!--<el-menu-item index="setting">设置</el-menu-item>-->
       </el-menu>
     </el-col>
   </el-row>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-import Setting from '@/components/Setting'
+// import Setting from '@/components/Setting'
 
 Vue.use(Router)
 
@@ -11,11 +11,11 @@ export default new Router({
       path: '/',
       name: '/',
       component: Home
-    },
-    {
-      path: '/setting',
-      name: 'setting',
-      component: Setting
     }
+    // {
+    //   path: '/setting',
+    //   name: 'setting',
+    //   component: Setting
+    // }
   ]
 })
