6f45dede36b6e4748882355e467be44b44e9d85a
ETY001 <ety001@domyself.me>
2017-07-27 11:05:33 +0800

    增加帮助文档

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -3,6 +3,7 @@
     <el-col :span="20" :offset="2">
       <el-menu theme="dark" :default-active="activeIndex" :router="true" class="el-menu-demo" mode="horizontal" @select="handleSelect">
         <el-menu-item index="/">首页</el-menu-item>
+        <li class="el-menu-item"><a href="https://shimo.im/doc/8eUIP28VaIwT9gAU?r=V7XL4/" target="_blank">教程</a></li>
         <!--<el-menu-item index="setting">设置</el-menu-item>-->
       </el-menu>
     </el-col>
@@ -31,3 +32,9 @@
     }
   }
 </script>
+
+<style >
+  .el-menu-item a {
+    text-decoration : none
+  }
+</style>
